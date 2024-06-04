0899c172b518363d21b0eb01dbd5dc874f7f5c6d
Marvin Josef Pick <marvinjosef.pick>
2024-06-04 16:56:18 +0200

    Initial commit

--- mergesort.py
+++ mergesort.py
@@ -1,39 +1,32 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
 def mergeSort(unsortedList):
-    if(len(unsortedList)>=1):
-    
-        mid = len(unsortedList) // 2
-        left = unsortedList[:mid]
-        right = unsortedList[mid:]
+  if len(unsortedList) >= 1:
+    mid = len(unsortedList) // 2
+    left = unsortedList[:mid]
+    right = unsortedList[mid:]
 
-        mergeSort(left)
-        mergeSort(right)
+    mergeSort(left)
+    mergeSort(right)
 
-        l = 0
-        r = 0
-        i = 0
+    l = r = 0
+    i = 0
+    while l < len(left) and r < len(right):
+      if left[l] <= right[r]:
+        unsortedList[i] = left[l]  # Direct assignment
+        l += 1
+      else:
+        unsortedList[i] = right[r]  # Direct assignment
+        r += 1
+      i += 1
 
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=unsortedList, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=unsortedList, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
+    while l < len(left):
+      unsortedList[i] = left[l]
+      l += 1
+      i += 1
 
-        while l < len(left):
-            unsortedList[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            unsortedList[i] = right[r]
-            r += 1
-            i += 1
+    while r < len(right):
+      unsortedList[i] = right[r]
+      r += 1
+      i += 1
 
 
 import matplotlib.pyplot as plt
