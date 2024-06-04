9ba89029731ad715ccf8327c123ff0317f5538b6
Marvin Josef Pick <marvinjosef.pick>
2024-06-04 16:27:30 +0200

    Initial commit

--- mergesort.py
+++ mergesort.py
@@ -2,15 +2,12 @@ def ASSIGNMENT(new_list, i, old_list, j):
     new_list[i] = old_list[j]
 
 
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
+def mergeSort(unsortedList):
+    if(len(unsortedList)>=1):
+    
+        mid = len(unsortedList) // 2
+        left = unsortedList[:mid]
+        right = unsortedList[mid:]
 
         mergeSort(left)
         mergeSort(right)
@@ -21,20 +18,20 @@ def mergeSort(list_to_sort_by_merge):
 
         while l < len(left) and r < len(right):
             if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
+                ASSIGNMENT(new_list=unsortedList, i=i, old_list=left, j=l)
                 l += 1
             else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
+                ASSIGNMENT(new_list=unsortedList, i=i, old_list=right, j=r)
                 r += 1
             i += 1
 
         while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
+            unsortedList[i] = left[l]
             l += 1
             i += 1
 
         while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
+            unsortedList[i] = right[r]
             r += 1
             i += 1
 
